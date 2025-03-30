f392cf918a6862b14e9df33b9b3eaee1426f781c
Michał Fereniec <michalkoks99@gmail.com>
2025-03-30 23:58:36 +0200

    Initial Commit

---
+++ PopularGithubProjects/PopularGithubProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularGithubProjectsApp.swift
+//  PopularGithubProjects
+//
+//  Created by Michal Fereniec on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PopularGithubProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PopularGithubProjectsTests/PopularGithubProjectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopularGithubProjectsTests.swift
+//  PopularGithubProjectsTests
+//
+//  Created by Michal Fereniec on 30/03/2025.
+//
+
+import Testing
+@testable import PopularGithubProjects
+
+struct PopularGithubProjectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
